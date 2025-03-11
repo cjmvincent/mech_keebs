033c6ab7113d8565d8c0a91c3ba6cc4275a0cb98
Charles Vincent <cjmvincent@outlook.com>
2025-03-11 07:44:37 -0400

    fix some keycodes

--- ballzout/keymaps/rev2/keymap.c
+++ ballzout/keymaps/rev2/keymap.c
@@ -35,9 +35,9 @@ combo_t key_combos[] = {
   //[COMBO_RCLICK] = COMBO(combo_rclick, KC_MS_BTN2),
 };
 
-layer_state_t layer_state_set_user(layer_state_t state) {
-    return update_tri_layer_state(state, 1, 2, 3);
-} 
+//layer_state_t layer_state_set_user(layer_state_t state) {
+//    return update_tri_layer_state(state, 1, 2, 4);
+//} 
 
 enum layer_names {
   _BASE,
@@ -51,7 +51,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     
     [_BASE] = LAYOUT(
         KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,                             KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,
-        LSFT_T(KC_A),   LCTL_T(KC_S),   LALT_T(KC_D),   LGUI_T(KC_F),   HYPR(KC_G),                       HYPR(KC_H),     LGUI_T(KC_J),   LALT_T(KC_K),   LCTL_T(KC_L),   LSFT_T(KC_SCLN),
+        LSFT_T(KC_A),   LCTL_T(KC_S),   LALT_T(KC_D),   LGUI_T(KC_F),   HYPR_T(KC_G),                     HYPR_T(KC_H),   LGUI_T(KC_J),   LALT_T(KC_K),   LCTL_T(KC_L),   LSFT_T(KC_SCLN),
         KC_Z,           KC_X,           KC_C,           KC_V,           KC_B,                             KC_N,           KC_M,           KC_COMM,        KC_DOT,         KC_SLSH,
         _______,        _______,        LT(1,KC_DEL),   LT(2,KC_BSPC),  KC_MUTE, KC_MS_BTN1, KC_MS_BTN2,  LT(3,KC_SPC),   LT(4,KC_ENT),   _______,        _______,        _______
     ),
@@ -77,7 +77,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         RGB_TOG,        _______,        _______,        _______,        _______,                          _______,        KC_F1,          KC_F2,          KC_F3,          KC_F4,
         RGB_VAI,        RGB_SAI,        RGB_HUI,        RGB_SPI,        RGB_MOD,                          _______,        KC_F5,          KC_F6,          KC_F7,          KC_F8,
         RGB_VAD,        RGB_SAD,        RGB_HUD,        RGB_SPD,        RGB_RMOD,                         _______,        KC_F9,          KC_F10,         KC_F11,         KC_F12,
-        _______,        _______,        _______,        _______,        _______, _______,    _______,     _______,        _______,        _______,        _______,        _______
+        _______,        _______,        _______,        _______,        _______, _______,    _______,      _______,        _______,        _______,        _______,        _______
     )
 };
 
