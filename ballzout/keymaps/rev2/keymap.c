d00a38301dba91ade7cb8c336ca0f84c92bb8a50
Charles Vincent <cjmvincent@outlook.com>
2025-03-22 11:12:14 -0400

    still figuring out my endgame keymap

--- ballzout/keymaps/rev2/keymap.c
+++ ballzout/keymaps/rev2/keymap.c
@@ -60,18 +60,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,                             KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,
         LSFT_T(KC_A),   LCTL_T(KC_S),   LALT_T(KC_D),   LGUI_T(KC_F),   HYPR_T(KC_G),                     HYPR_T(KC_H),   RGUI_T(KC_J),   RALT_T(KC_K),   RCTL_T(KC_L),   RSFT_T(KC_SCLN),
         KC_Z,           KC_X,           KC_C,           KC_V,           KC_B,                             KC_N,           KC_M,           KC_COMM,        KC_DOT,         KC_SLSH,
-        _______,        _______,        LT(2,KC_DEL),   LT(1,KC_BSPC),  KC_MUTE, KC_MS_BTN1, KC_MS_BTN2,  LT(3,KC_SPC),   MO(4),          ______,        _______,        _______
+        _______,        _______,        LT(2,KC_DEL),   LT(1,KC_BSPC),  KC_MUTE, KC_MS_BTN1, KC_MS_BTN2,  LT(3,KC_SPC),   _______,        _______,        _______,        _______
     ),
     [_FN1] = LAYOUT(
         _______,        _______,        _______,        _______,        _______,                          KC_NUM,         KC_7,           KC_8,           KC_9,           KC_PMNS,
         _______,        _______,        _______,        _______,        _______,                          KC_PSLS,        KC_4,           KC_5,           KC_6,           KC_PPLS,
-        _______,        _______,        _______,        _______,        _______,                          KC_PAST,        KC_1,           KC_2,           KC_3,           KC_PEQL,
-        _______,        _______,        _______,        _______,        _______, _______,    _______,     KC_ENT,         KC_0,           KC_PDOT,        _______,        _______
+        _______,        _______,        _______,        _______,        _______,                          KC_PAST,        KC_1,           KC_2,           KC_3,           KC_PDOT,
+        _______,        _______,        _______,        _______,        _______, _______,    _______,     KC_PEQL,        KC_0,           _______,        _______,        _______
     ),
     [_FN2] = LAYOUT(
-        KC_TAB,         _______,        _______,        _______,        _______,                          KC_HOME,        KC_PGDN,        KC_PGUP,        KC_END,         _______,
-        KC_CAPS,        _______,        _______,        _______,        _______,                          KC_LEFT,        KC_DOWN,        KC_UP,          KC_RGHT,        _______,
-        KC_ESC,         _______,        _______,        _______,        _______,                          _______,        _______,        _______,        _______,        _______,
+        _______,        _______,        _______,        _______,        _______,                          KC_HOME,        KC_PGDN,        KC_PGUP,        KC_END,         _______,
+        _______,        _______,        _______,        _______,        _______,                          KC_LEFT,        KC_DOWN,        KC_UP,          KC_RGHT,        _______,
+        _______,        _______,        _______,        _______,        _______,                          _______,        _______,        _______,        _______,        _______,
         _______,        _______,        _______,        _______,        _______, _______,    _______,     _______,        _______,        _______,        _______,        _______
     ),
     [_FN3] = LAYOUT(
