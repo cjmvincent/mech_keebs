dc3efc8c6ee134d76c8191c7958c14bdcad8d444
Charles Vincent <cjmvincent@outlook.com>
2025-07-11 14:48:00 -0400

    change tapping term bc I keep making mistakes

--- ballzout/config.h
+++ ballzout/config.h
@@ -18,7 +18,7 @@
 
 /* tapping term */
 // #define TAPPING_FORCE_HOLD
-//#define TAPPING_TERM 200
+#define TAPPING_TERM 300
 // #define IGNORE_MOD_TAP_INTERRUPT
 
 #define RP2040_BOOTLOADER_DOUBLE_TAP_RESET // Activates the double-tap behavior
