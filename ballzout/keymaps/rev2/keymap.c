eb55718e38c6f0b6151ed145ba5523d56016e2a3
Charles Vincent <cjmvincent@outlook.com>
2026-07-20 14:17:02 -0400

    trying a different HRW layout

--- ballzout/keymaps/rev2/keymap.c
+++ ballzout/keymaps/rev2/keymap.c
@@ -64,8 +64,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     
     [_BASE] = LAYOUT(
         KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,                             KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,
-        LSFT_T(KC_A),   LCTL_T(KC_S),   LALT_T(KC_D),   LGUI_T(KC_F),   HYPR_T(KC_G),                     HYPR_T(KC_H),   RGUI_T(KC_J),   RALT_T(KC_K),   RCTL_T(KC_L),   RSFT_T(KC_SCLN),
-        KC_Z,           KC_X,           KC_C,           KC_V,           KC_B,                             KC_N,           KC_M,           KC_COMM,        KC_DOT,         KC_SLSH,
+        LCTL_T(KC_A),   LALT_T(KC_S),   LGUI_T(KC_D),   LSFT_T(KC_F),   KC_G,                             KC_H,           RSFT_T(KC_J),   RGUI_T(KC_K),   RALT_T(KC_L),   RCTL_T(KC_SCLN),
+        HYPR_T(KC_Z),   KC_X,           KC_C,           KC_V,           KC_B,                             KC_N,           KC_M,           KC_COMM,        KC_DOT,         HYPR_T(KC_SLSH),
         _______,        QK_LLCK,        LT(2,KC_DEL),   LT(1,KC_BSPC),  KC_MUTE, MS_BTN1,    MS_BTN2,     LT(3,KC_SPC),   _______,        QK_LLCK,        _______,        _______
     ),
     [_FN1] = LAYOUT(
