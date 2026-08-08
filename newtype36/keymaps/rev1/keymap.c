4b830b0b0a49f903ae9f33f95d178f6ef01c83f1
Charles Vincent <cjmvincent@outlook.com>
2026-08-08 12:59:49 -0400

    forgot hyper key

--- newtype36/keymaps/rev1/keymap.c
+++ newtype36/keymaps/rev1/keymap.c
@@ -65,7 +65,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] = LAYOUT(
         KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,           KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,
         LCTL_T(KC_A),   LALT_T(KC_S),   LGUI_T(KC_D),   LSFT_T(KC_F),   (KC_G),         (KC_H),         RSFT_T(KC_J),   RGUI_T(KC_K),   RALT_T(KC_L),   RCTL_T(KC_SCLN),
-        KC_Z,           KC_X,           KC_C,           KC_V,           KC_B,           KC_N,           KC_M,           KC_COMM,        KC_DOT,         KC_SLSH,
+        HYPR_T(KC_Z),   KC_X,           KC_C,           KC_V,           KC_B,           KC_N,           KC_M,           KC_COMM,        KC_DOT,         HYPR_T(KC_SLSH),
                                         QK_LLCK,        LT(2,KC_DEL),   LT(1,KC_BSPC),  LT(3,KC_SPC),   _______,        QK_LLCK
     ),
     [_FN1] = LAYOUT(
